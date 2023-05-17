33b5d76301ed07286c0cc35dfd37a05432d13633
S-H-GAMELINKS <gamelinks007@gmail.com>
2023-05-17 20:40:27 +0900

    Fix report target(ignore to local user followed account)

--- sub.py
+++ sub.py
@@ -22,7 +22,7 @@ def download_file(url, dst_path):
             with open(dst_path, mode='wb') as local_file:
                 local_file.write(data)
     except urllib.error.URLError as e:
-        print(e)
+        pass
 
 # 連合タイムラインをListenするためのクラスを定義
 class PublicStreamListener(StreamListener):
@@ -36,50 +36,47 @@ class PublicStreamListener(StreamListener):
         try:
             super().handle_stream(response)
         except:
-            print(response)
-            print("Error!")
+            pass
 
     def on_update(self, status):
-        # 投稿の画像データとNSFWフラグを取得
-        media_attachments = status.get('media_attachments')
+        try:
+            # 投稿の画像データとNSFWフラグを取得
+            media_attachments = status.get('media_attachments')
 
-        # 画像をチェック
-        if len(media_attachments) > 0:
-            for media_attachment in media_attachments:
-                # ログに画像のURLを出力
-                print(media_attachment.url)
+            # 画像をチェック
+            if len(media_attachments) > 0:
+                for media_attachment in media_attachments:
+                    # ログに画像のURLを出力
+                    print(media_attachment.url)
 
-                # 画像を一時的に保存
-                dst_path = './nsfw'
-                download_file(media_attachment.url, dst_path)
+                    # 画像を一時的に保存
+                    dst_path = './nsfw'
+                    download_file(media_attachment.url, dst_path)
 
-                # 画像がNSFWなものかをチェックし、ダウンロードした画像は削除
-                result = n2.predict_image(dst_path)
-                os.remove(dst_path)
+                    # 画像がNSFWなものかをチェックし、ダウンロードした画像は削除
+                    result = n2.predict_image(dst_path)
+                    os.remove(dst_path)
 
-                # チェック結果をログに出力
-                print(result)
+                    # チェック結果をログに出力
+                    print(result)
 
-                # 基準値以上の場合は通報
-                if result > 0.90:
-                    # 投稿のIDとアカウントを取得
-                    statusID = status.id
-                    account = status.account
+                    # 基準値以上の場合は通報
+                    if result:
+                        # 投稿のIDとアカウントを取得
+                        statusID = status.id
+                        account = status.account
 
-                    # ログにアカウント情報などを出力
-                    print(account.id)
-                    print(self.client.account_followers(account.id))
+                        # 通報対象の投稿をしたアカウントをフォローしているユーザーがいるかを取得
+                        target_account_followers = self.client.account_followers(account.id)
 
-                    # 通報対象の投稿をしたアカウントをフォローしているユーザーがいるかを取得
-                    target_account_followers = self.client.account_followers(account.id)
-
-                    # 通報を行い、該当の投稿を自動削除
-                    # 条件：対象の投稿をしたアカウントをフォローしているユーザーがサーバ内にいない
-                    if len(target_account_followers) == 0:
-                        self.client.report(account.id, status_ids=[statusID])
-                        self.client.status_delete(statusID)
-
-                    break
+                        # 通報を行う
+                        # 条件：対象の投稿をしたアカウントをフォローしているユーザーがサーバ内にいない場合のみ
+                        if len(target_account_followers) == 0:
+                            report = self.client.report(account.id, status_ids=[statusID])
+                        break
+        except Exception as e:
+            print(e)
+            pass
 
 # 連合タイムラインのListenerを生成            
 public_stream_listener = PublicStreamListener(client)
